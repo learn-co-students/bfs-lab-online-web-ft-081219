e94e649adc34981e5714e6908cdfa361340cb1bc
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-09 20:31:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-function bfs(rootNode, vertices, edges){
+function findAdjacent(verticeName, vertices, edges){
+  let adjacentNames = [];
+  for( let i in edges){
+    if(edges[i].includes(verticeName)){
+      adjacentNames.push(edges[i].find(name => name != verticeName))
+    };
+  };
+  return vertices.filter(v => adjacentNames.includes(v.name) && v.distance === null)
+};
 
-}
+function markDistanceAndPredecessor(hearNode, adjacentNodes){
+  adjacentNodes.forEach(node => {
+    node.distance = 1;
+    node.predecessor = hearNode;
+  });
+};
+
+function bfs(rootNode, vertices, edges){
+  let nodesArr = [];
+
+  function addToNodesArr(nodeToAdd){
+    nodesArr.push(nodeToAdd);
+  };
+
+  addToNodesArr(rootNode);
+
+  while(nodesArr.length > 0){
+    let firstNode = nodesArr.shift();
+    let adjacentVertices = findAdjacent(firstNode, vertices, edges);
+    if (adjacentVertices){
+      markDistanceAndPredecessor(firstNode, adjacentVertices);
+      for(let i in adjacentVertices){
+        addToNodesArr(adjacentVertices[i]);
+      };
+    };
+  };
+  return vertices;
+};
